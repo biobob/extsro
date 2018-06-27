799efbd29a610cfd89608bec9127896a7ebfb223
biobob <robert.komorovsky@gmail.com>
2018-06-27 15:32:41 +0200

    Updated build script to process HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 // grab gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    
+// configuration
+    conf = {
+      paths: {
+        src: {
+          htm: './src/html/*.html',
+          css: './src/css/*.css',
+          php: './src/php/*.php',
+          img: './src/img/*.*',
+          res: './src/res/*.*'
+        },
+        dist: './dist'
+      }
+    };
 
 // create a default task
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!');
+gulp.task('default', ['html']);
+
+// process HTML files
+gulp.task('html', function() {
+  gulp.src(conf.paths.src.htm)
+      .pipe(gulp.dest(conf.paths.dist));
 });
