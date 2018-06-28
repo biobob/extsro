e4c9daf902d5574efc8ee17ab6951f9b63d167aa
biobob <robert.komorovsky@gmail.com>
2018-06-28 02:09:07 +0200

    ES6 format

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,63 @@
-const gulp  = require('gulp'),
-    debug = require('gulp-debug'),
-    log = require('fancy-log'),
-    ftp = require('vinyl-ftp'),
+import gulp from 'gulp';
+import debug from 'gulp-debug';
+import log from 'fancy-log';
+import ftp from 'vinyl-ftp';
     
-    conf = {
-      paths: {
+const conf = {
+    paths: {
         src: {
-          htm: './src/html/*.html',
-          css: './src/css/*.css',
-          php: './src/php/*.php',
-          img: './src/img/*.*',
-          res: './src/res/*.*'
+            htm: './src/html/*.html',
+            css: './src/css/*.css',
+            php: './src/php/*.php',
+            img: './src/img/*.*',
+            res: './src/res/*.*'
         },
         dist: './dist'
-      }
-    };
+    }
+};
 
-// default task
-gulp.task('default', gulp.series(['html', 'deploy']));
-
-// process HTML files
-gulp.task('html', () => {
-  gulp.src(conf.paths.src.htm)
+const html = () => {
+    return gulp.src(conf.paths.src.htm)
       .pipe(debug({title: 'html:'}))
       .pipe(gulp.dest(conf.paths.dist));
-});
+}
 
-// deploy via FTP
-gulp.task('deploy', () => {
-  var conn = ftp.create({
+const deploy = () => {
+    const conn = ftp.create({
         host:        process.env.FTP_HOST,
         user:        process.env.FTP_USER,
         password:    process.env.FTP_PASS,
         log:         log,
         idleTimeout: 10000
-      });
-    
-  //conn.clean([process.env.FTP_PATH + '/**'], conf.paths.dist, {base: '/'});
+    });
+    return gulp.src([conf.paths.dist + '/**'], {base: conf.paths.dist, buffer: false})
+        .pipe(debug({title: 'ftp:'}))
+        .pipe(conn.dest(process.env.FTP_PATH));
+}
 
-  gulp.src([conf.paths.dist + '/**'], {base: conf.paths.dist, buffer: false})
-      .pipe(debug({title: 'ftp:'}))
-      .pipe(conn.dest(process.env.FTP_PATH));
-});
+// default task (called from CLI when executing `gulp`)
+gulp.task('default', gulp.series(html, deploy));
+
+// process HTML files
+//gulp.task('html', () => {
+//  gulp.src(conf.paths.src.htm)
+//      .pipe(debug({title: 'html:'}))
+//      .pipe(gulp.dest(conf.paths.dist));
+//});
+//
+// deploy via FTP
+//gulp.task('deploy', () => {
+//  var conn = ftp.create({
+//        host:        process.env.FTP_HOST,
+//        user:        process.env.FTP_USER,
+//        password:    process.env.FTP_PASS,
+//        log:         log,
+//        idleTimeout: 10000
+//      });
+//    
+//  //conn.clean([process.env.FTP_PATH + '/**'], conf.paths.dist, {base: '/'});
+//
+//  gulp.src([conf.paths.dist + '/**'], {base: conf.paths.dist, buffer: false})
+//      .pipe(debug({title: 'ftp:'}))
+//      .pipe(conn.dest(process.env.FTP_PATH));
+//});
